a807c5e111c9564f3c3f684129e21b539b480555
sarassaura <sarahyukinonakada@gmail.com>
2023-10-06 12:30:23 -0300

    screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <AppShell slotPageFooter="flex justify-center py-5 bg-surface-500">
-	<div class="h-64 bg-surface-500" />
+	<div class="h-screen bg-surface-500" />
 	<AppBar class="sticky top-0">Page Header</AppBar>
 	<slot />
 	<svelte:fragment slot="pageFooter">Footer</svelte:fragment>
