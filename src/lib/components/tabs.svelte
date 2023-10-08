37e016ce1eaf0941e66886d867629f5b822ec46d
sarassaura <sarahyukinonakada@gmail.com>
2023-10-08 16:02:50 -0300

    navbar direction aware

--- src/lib/components/tabs.svelte
+++ src/lib/components/tabs.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
 	import { TabGroup, TabAnchor } from '@skeletonlabs/skeleton';
+	import { onMount } from 'svelte';
+
 	const links = ['about', 'skills', 'projects', 'services', 'contact-me'];
+	let TabList: HTMLElement;
+
+	onMount(() => {
+		TabList = document.querySelector('.tab-list')!;
+	});
+
+	function change(id: number) {
+		const offsetLeft = TabList.querySelectorAll('a')[id].offsetLeft;
+		const width = TabList.querySelectorAll('a')[id].offsetWidth / TabList.offsetWidth;
+		TabList.style.setProperty('--_left', offsetLeft + 'px');
+		TabList.style.setProperty('--_width', width.toString());
+	}
 </script>
 
 <TabGroup border="border-none">
-	{#each links as link}
-		<TabAnchor name="tab1" href={`#${link}`} class="uppercase">{link.replace('-', ' ')}</TabAnchor>
+	{#each links as link, id}
+		<TabAnchor name={`tab${id + 1}`} href={`#${link}`} class="uppercase" on:click={() => change(id)}
+			>{link.replace('-', ' ')}
+		</TabAnchor>
 	{/each}
 </TabGroup>
