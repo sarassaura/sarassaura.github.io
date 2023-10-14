1fc64c0e2e523fb4d7dbcf7e551d3de34648b97e
sarassaura <sarahyukinonakada@gmail.com>
2023-10-14 16:09:54 -0300

    form

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,1 +1,20 @@
-<div class="section contact" id="contact-me">Contact</div>
+<div class="section contact" id="contact-me">
+	<form class="p-4">
+		<label class="label row-start-2">
+			<span>Name</span>
+			<input class="input" type="text" />
+		</label>
+		<label class="label row-start-3">
+			<span>Email</span>
+			<input class="input" type="email" />
+		</label>
+		<label class="label row-start-4">
+			<span>Message</span>
+			<textarea class="textarea" rows="5" />
+		</label>
+		<label class="label row-start-5 flex justify-center">
+			<button class="btn variant-filled">Send</button>
+		</label>
+	</form>
+	<div class="phone" />
+</div>
