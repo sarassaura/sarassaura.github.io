701649d1189747b75be3bcf5c72633251ba24731
sarassaura <sarahyukinonakada@gmail.com>
2023-10-14 20:11:43 -0300

    button useful

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -5,8 +5,8 @@
 	<div class="presentation overflow-hidden md:px-16">
 		<p class="text-4xl pb-8 w-fit">Hi, I'm Sarah a Web Developer</p>
 		<div>
-			<button class="btn variant-filled">Resume</button>
-			<button class="btn variant-ghost-tertiary">Email me</button>
+			<a class="btn variant-filled" href="https://rxresu.me/sarahyukino/sarah-nakada">Resume</a>
+			<a class="btn variant-ghost-tertiary" href="mailto:sarahyukinonakada@gmail.com">Email me</a>
 		</div>
 	</div>
 	<div class="image">
