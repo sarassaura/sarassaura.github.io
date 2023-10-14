5b969ba9a0c3855c3ec394d5b2c1e66f3d2260a5
sarassaura <sarahyukinonakada@gmail.com>
2023-10-14 13:02:13 -0300

    media queries

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -23,14 +23,14 @@
 
 <div class="section projects" id="projects">
 	{#each data.data.viewer.pinnedItems.edges as item}
-		<p>{item.node.name}</p>
-		<p>{item.node.description}</p>
-		<p>{item.node.openGraphImageUrl}</p>
-		<p>{item.node.createdAt}</p>
-		<p>{item.node.homepageUrl}</p>
-		{#each item.node.repositoryTopics.nodes as topics}
-			<p>{topics.topic.name}</p>
-		{/each}
-		<br />
+		<div class="work" style={`background-image: url(${item.node.openGraphImageUrl})`}>
+			<p>{item.node.name}</p>
+			<p>{item.node.description}</p>
+			<p>{item.node.createdAt}</p>
+			<p>{item.node.homepageUrl}</p>
+			{#each item.node.repositoryTopics.nodes as topics}
+				<p>{topics.topic.name}</p>
+			{/each}
+		</div>
 	{/each}
 </div>
