782862429bc6d2e25b489b66cd2f483247f59ebf
sarassaura <sarahyukinonakada@gmail.com>
2023-10-18 20:02:57 -0300

    carousel

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,28 +1,48 @@
 <script lang="ts">
-	import { section } from '$lib';
 	import { fade } from 'svelte/transition';
 
-	let now: string;
+	let SliderRef: HTMLDivElement;
 
-	section.subscribe((value) => {
-		now = value;
-	});
+	function GoLeft() {
+		const x =
+			SliderRef.scrollLeft == 0
+				? SliderRef.clientWidth * SliderRef.childElementCount
+				: SliderRef.scrollLeft - SliderRef.clientWidth;
+		SliderRef.scroll(x, 0);
+	}
+	function GoRight() {
+		const x =
+			SliderRef.scrollLeft == SliderRef.scrollWidth - SliderRef.clientWidth
+				? 0
+				: SliderRef.scrollLeft + SliderRef.clientWidth;
+		SliderRef.scroll(x, 0);
+	}
 </script>
 
 <div class="section about" id="about">
-	{#if now == 'about' || now == ''}
-		<div class="presentation overflow-hidden md:px-16 px-4 gap-y-5 md:gap-y-20">
-			<p class="h6 w-fit title" transition:fade={{ duration: 500, delay: 500 }}>
-				Hi, I'm Sarah a <br class="break" />
-				<span class="typewrite">Web Developer</span>
-			</p>
-			<div transition:fade={{ duration: 500, delay: 500 }}>
-				<a class="btn variant-filled" href="https://rxresu.me/sarahyukino/sarah-nakada">Resume</a>
-				<a class="btn variant-ghost-tertiary" href="mailto:sarahyukinonakada@gmail.com">Email me</a>
-			</div>
+	<div class="presentation overflow-hidden md:px-16 px-4 gap-y-5 md:gap-y-20">
+		<p class="h6 w-fit title" transition:fade={{ duration: 500, delay: 500 }}>
+			Hi, I'm Sarah a <br class="break" />
+			<span class="typewrite">Web Developer</span>
+		</p>
+		<div transition:fade={{ duration: 500, delay: 500 }}>
+			<a class="btn variant-filled" href="https://rxresu.me/sarahyukino/sarah-nakada">Resume</a>
+			<a class="btn variant-ghost-tertiary" href="mailto:sarahyukinonakada@gmail.com">Email me</a>
 		</div>
-		<div class="image" transition:fade={{ duration: 500, delay: 1000 }}>
+	</div>
+	<div class="carousel" transition:fade={{ duration: 500, delay: 1000 }}>
+		<div class="slider" bind:this={SliderRef}>
+			<img src="/iphone.webp" alt="iphone" />
+			<img src="/iphone.webp" alt="iphone" />
 			<img src="/iphone.webp" alt="iphone" />
 		</div>
-	{/if}
+		<div class="controls">
+			<button class="prev btn variant-ghost" on:click={GoLeft}>
+				<iconify-icon icon="uiw:left" height="36" />
+			</button>
+			<button class="next btn variant-ghost" on:click={GoRight}>
+				<iconify-icon icon="uiw:right" height="36" />
+			</button>
+		</div>
+	</div>
 </div>
