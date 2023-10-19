82ad846ceb7a83d1de0997adbc14f6a04d607d7c
sarassaura <sarahyukinonakada@gmail.com>
2023-10-19 13:37:11 -0300

    resume

--- src/routes/(resume)/resume/+page.svelte
+++ src/routes/(resume)/resume/+page.svelte
@@ -1,1 +1,28 @@
-<div class="bg-red-500">resume</div>
+<div class="container">
+	<div class="flex w-full justify-around py-4">
+		<a class="btn variant-filled" href="/">Come back</a>
+		<button class="btn variant-filled">Switch Languages</button>
+		<a class="btn variant-filled" href="/resume.pdf" download="sarah_nakada">Download PDF</a>
+	</div>
+	<div class="wrapper">
+		<img src="resume1.webp" alt="resume" />
+	</div>
+</div>
+
+<style>
+	.container {
+		display: contents;
+		align-self: center;
+		align-content: center;
+	}
+	.wrapper {
+		width: 100%;
+		height: calc(100% - 80px);
+		padding: 1rem 1rem;
+	}
+	img {
+		width: 100%;
+		height: 100%;
+		object-fit: contain;
+	}
+</style>
