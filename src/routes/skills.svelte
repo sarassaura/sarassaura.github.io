fdc91dc816c0dcfef969d1ead6464ee3737bee1f
sarassaura <sarahyukinonakada@gmail.com>
2023-10-18 21:55:15 -0300

    cards

--- src/routes/skills.svelte
+++ src/routes/skills.svelte
@@ -4,9 +4,21 @@
 
 <div class="section skills md:px-16 px-4 flex flex-col items-center justify-center" id="skills">
 	<ShowOff />
-	<div class="flex flex-row gap-x-4">
-		<div>Frontend</div>
-		<div>Backend</div>
-		<div>Design</div>
+	<div class="know grid md:gap-x-4 gap-y-4">
+		<div class="card card-hover variant-ringed-primary block p-4">
+			<header class="card-header">Frontend</header>
+			<section class="p-4">(content)</section>
+			<footer class="card-footer">(footer)</footer>
+		</div>
+		<div class="card card-hover variant-ringed-primary block p-4">
+			<header class="card-header">Backend</header>
+			<section class="p-4">(content)</section>
+			<footer class="card-footer">(footer)</footer>
+		</div>
+		<div class="card card-hover variant-ringed-primary block p-4">
+			<header class="card-header">Design</header>
+			<section class="p-4">(content)</section>
+			<footer class="card-footer">(footer)</footer>
+		</div>
 	</div>
 </div>
