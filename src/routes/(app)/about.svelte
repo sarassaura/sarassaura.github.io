c2fdc165e91d5df893d1cc52de7630db8abc7685
sarassaura <sarahyukinonakada@gmail.com>
2023-10-20 16:37:25 -0300

    subheading

--- src/routes/(app)/about.svelte
+++ src/routes/(app)/about.svelte
@@ -4,13 +4,17 @@
 	let SliderRef: HTMLDivElement;
 </script>
 
-<div class="section about" id="about">
-	<div class="presentation overflow-hidden md:px-16 px-4 gap-y-5 md:gap-y-20">
+<div class="section about shrink-none" id="about">
+	<div class="presentation overflow-hidden md:px-16 px-4 gap-y-5">
 		<p class="h6 w-fit title text-center">
 			Hi, I'm Sarah a<br class="break" />
 			<span class="typewrite">Web Developer</span>
 		</p>
-		<div class="flex gap-2 md:gap-4">
+		<div class="paragraph text-center text-base">
+			I am passionate about coding with a love for programming. I have a technical diploma in
+			computing for the internet and I'm currently studying computer science.
+		</div>
+		<div class="flex gap-2 md:gap-4 mt-8">
 			<a class="btn variant-filled" href={`${$page.url.origin}/resume`}>Resume</a>
 			<a class="btn variant-ghost-tertiary" href={`${$page.url.origin}/#contact-me`}>Email me</a>
 		</div>
