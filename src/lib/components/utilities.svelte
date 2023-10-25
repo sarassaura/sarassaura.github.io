c8a3283675cee9f5cfcb5792618689f9b3dfe0cf
sarassaura <sarahyukinonakada@gmail.com>
2023-10-25 19:36:39 -0300

    localstorage

--- src/lib/components/utilities.svelte
+++ src/lib/components/utilities.svelte
@@ -3,12 +3,22 @@
 	import { popup } from '@skeletonlabs/skeleton';
 	import { onMount } from 'svelte';
 
-	let colorValue: string = '#0EA5E9';
+	let colorValue: string;
 	let root: HTMLBodyElement;
 
 	onMount(() => {
 		root = document.getElementsByTagName('body')[0];
+		colorValue = window.localStorage.getItem('color') || '#0EA5E9';
+		root.style.setProperty('--color-tertiary-500', rgb(colorValue));
 	});
+
+	function rgb(hex: string) {
+		const color = hex;
+		const r = parseInt(color.slice(1, 3), 16);
+		const g = parseInt(color.slice(3, 5), 16);
+		const b = parseInt(color.slice(5, 7), 16);
+		return `${r} ${g} ${b}`;
+	}
 </script>
 
 <div class="card p-4 variant-glass-tertiary rounded-md" data-popup="config">
@@ -24,12 +34,8 @@
 				type="color"
 				bind:value={colorValue}
 				on:change={() => {
-					const color = colorValue;
-					const r = parseInt(color.slice(1, 3), 16);
-					const g = parseInt(color.slice(3, 5), 16);
-					const b = parseInt(color.slice(5, 7), 16);
-
-					root.style.setProperty('--color-tertiary-500', `${r} ${g} ${b}`);
+					root.style.setProperty('--color-tertiary-500', rgb(colorValue));
+					window.localStorage.setItem('color', colorValue);
 				}}
 			/>
 		</div>
@@ -39,6 +45,7 @@
 			class="btn variant-outline-tertiary"
 			on:click={() => {
 				colorValue = '#0EA5E9';
+				window.localStorage.setItem('color', colorValue);
 				root.style.setProperty('--color-tertiary-500', `14 165 233`);
 			}}>Reset</button
 		>
