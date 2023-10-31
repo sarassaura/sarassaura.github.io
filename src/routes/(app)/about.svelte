14574979a444e76c55a0ee7abbac56704c89e7b1
sarassaura <sarahyukinonakada@gmail.com>
2023-10-31 00:43:22 -0300

    correcting ripple

--- src/routes/(app)/about.svelte
+++ src/routes/(app)/about.svelte
@@ -17,7 +17,7 @@
 		</div>
 		<div class="flex gap-2 md:gap-3 mt-4 w-fit">
 			<a class="btn variant-filled but" href={`/resume`} on:click={Ripple}>Resume</a>
-			<a class="btn variant-ghost-primary but" href={`/#contact-me`} on:click={Ripple}>Email me</a>
+			<a class="btn variant-ghost-primary" href={`/#contact-me`}>Email me</a>
 		</div>
 	</div>
 	<Hero />
