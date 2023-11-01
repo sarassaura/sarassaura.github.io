65de3a6799dbdddec145935c23fbae7b6e98f6f9
sarassaura <sarahyukinonakada@gmail.com>
2023-10-31 23:23:03 -0300

    replace tablist

--- src/lib/components/tabs.svelte
+++ src/lib/components/tabs.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { change, unchange } from '$lib';
-	import { TabGroup, TabAnchor } from '@skeletonlabs/skeleton';
 	import { onMount } from 'svelte';
 
 	const links = ['about', 'skills', 'projects', 'contact-me'];
@@ -13,16 +12,15 @@
 	});
 </script>
 
-<TabGroup border="border-none relative" class="hidden lg:flex">
+<div class="hidden lg:flex border-none relative blue tab-list">
 	{#each links as link, id}
-		<TabAnchor
-			name={`tab${id + 1}`}
-			controls={`tab${id + 1}`}
+		<a
 			href={`${$page.url.origin}/#${link}`}
-			class="capitalize"
+			class="capitalize h-full py-2 px-4"
 			on:mouseover={() => change(id, TabList)}
+			on:focus={() => {}}
 			on:mouseleave={() => unchange(TabList, links, $page.url.hash)}
 			>{link.replace('-', ' ')}
-		</TabAnchor>
+		</a>
 	{/each}
-</TabGroup>
+</div>
