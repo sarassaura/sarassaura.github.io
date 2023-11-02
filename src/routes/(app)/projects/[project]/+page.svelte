a45ebf07ab84eeb8e2e04b8db9d7efdecd217471
sarassaura <sarahyukinonakada@gmail.com>
2023-11-01 21:56:16 -0300

    style markdown

--- src/routes/(app)/projects/[project]/+page.svelte
+++ src/routes/(app)/projects/[project]/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { page } from '$app/stores';
 	import { Ripple } from '$lib/functions/ripple';
 
 	/** @type {import('./$types').PageData} */
@@ -6,10 +7,16 @@
 </script>
 
 <div class="flex w-full justify-around py-4">
-	<a class="btn variant-filled px-3 py-3 but" href="/" on:click={Ripple}>
+	<a
+		class="btn variant-filled px-3 py-3 but"
+		href={`${$page.url.origin}/#projects`}
+		on:click={Ripple}
+	>
 		<iconify-icon icon="mdi:arrow-left" height="28" width="28" />
 		<span class="break">&nbspGo back&nbsp</span>
 	</a>
 </div>
 
-{@html data.data}
+<div class="mdx px-4 md:px-16">
+	{@html data.data}
+</div>
