ae5b3419fa019db8fca9b3091602f7e64b3c3ec1
sarassaura <sarahyukinonakada@gmail.com>
2023-11-06 02:25:43 -0300

    loader

---
+++ docs/_app/immutable/nodes/6.2a4dd528.js
@@ -0,0 +1,1 @@
+import{L as n}from"../chunks/layout.37f5f790.js";const e=!0,o=Object.freeze(Object.defineProperty({__proto__:null,prerender:e},Symbol.toStringTag,{value:"Module"}));export{n as component,o as universal};
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,15 +5,22 @@
 	import 'iconify-icon';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	import { sineIn, sineOut } from 'svelte/easing';
+	import { onMount } from 'svelte';
 
 	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
 
 	export let data: { url: string };
+
+	let mount = false;
+	onMount(() => (mount = true));
 </script>
 
 {#key data.url}
+	<div class={`w-full h-full items-center justify-center ${mount ? 'hidden' : 'flex'}`}>
+		Loading...
+	</div>
 	<div
-		class="w-full h-full flex flex-col"
+		class={`w-full h-full flex-col ${mount ? 'flex' : 'hidden'}`}
 		in:fly={{ x: -100, duration: 500, delay: 500, easing: sineIn }}
 		out:fly={{ x: 100, duration: 500, easing: sineOut }}
 	>
