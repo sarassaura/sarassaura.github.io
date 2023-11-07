a59c11652b740338a1762a7d6f1fc3ef66060aed
sarassaura <sarahyukinonakada@gmail.com>
2023-11-06 22:39:44 -0300

    loading animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,9 +21,17 @@
 </script>
 
 {#key data.url}
-	<div class={`w-full h-full items-center justify-center ${mount && !wait ? 'hidden' : 'flex'}`}>
-		Loading...
+	<div
+		class={`w-full h-full items-center justify-center relative ${
+			mount && !wait ? 'hidden' : 'flex'
+		}`}
+	>
+		<div class="loading" />
+		<div class="loading" />
+		<div class="loading" />
+		<span>Loading...</span>
 	</div>
+
 	<div
 		class={`w-full h-full flex-col ${mount && !wait ? 'flex' : 'hidden'}`}
 		in:fly={{ x: -100, duration: 500, delay: 500, easing: sineIn }}
@@ -32,3 +40,55 @@
 		<slot />
 	</div>
 {/key}
+
+<style>
+	.loading {
+		position: absolute;
+		height: 200px;
+		width: 200px;
+		border-color: currentColor;
+		border-radius: 50%;
+	}
+
+	.loading:nth-child(1) {
+		animation: circle1 2s linear infinite;
+		border-bottom: 8px solid;
+	}
+
+	.loading:nth-child(2) {
+		animation: circle2 2s linear infinite;
+		border-right: 8px solid;
+	}
+
+	.loading:nth-child(3) {
+		animation: circle3 2s linear infinite;
+		border-top: 8px solid;
+	}
+
+	@keyframes circle1 {
+		0% {
+			transform: rotateX(35deg) rotateY(-45deg) rotateZ(0deg);
+		}
+		100% {
+			transform: rotateX(35deg) rotateY(-45deg) rotateZ(360deg);
+		}
+	}
+
+	@keyframes circle2 {
+		0% {
+			transform: rotateX(50deg) rotateY(10deg) rotateZ(0deg);
+		}
+		100% {
+			transform: rotateX(50deg) rotateY(10deg) rotateZ(360deg);
+		}
+	}
+
+	@keyframes circle3 {
+		0% {
+			transform: rotateX(35deg) rotateY(55deg) rotateZ(0deg);
+		}
+		100% {
+			transform: rotateX(35deg) rotateY(55deg) rotateZ(360deg);
+		}
+	}
+</style>
