4041fb1bf92c5f72121ace394944f74c16b39fa3
sarassaura <sarahyukinonakada@gmail.com>
2023-11-15 13:15:03 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/lib/components/tabs.svelte
+++ src/lib/components/tabs.svelte
@@ -1,19 +1,14 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { change, unchange } from '$lib';
-	import { onMount } from 'svelte';
 	import { locale, LL } from '$lib/i18n/i18n-svelte';
 
 	let links = Object.keys($LL.tabs);
 
 	let TabList: HTMLElement | null;
-
-	onMount(() => {
-		TabList = document.querySelector('.tab-list');
-	});
 </script>
 
-<div class="hidden lg:flex border-none relative blue tab-list">
+<div class="hidden lg:flex border-none relative blue tab-list" bind:this={TabList}>
 	{#each links as link, id}
 		<button
 			class="capitalize h-full py-2 px-4"
--- src/routes/[[lang=lang]]/(app)/+page.svelte
+++ src/routes/[[lang=lang]]/(app)/+page.svelte
@@ -1,51 +1,13 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import About from './about.svelte';
 	import Contact from './contact.svelte';
 	import Projects from './projects.svelte';
 	import Skills from './skills.svelte';
-	import { Footer, change, change_id } from '$lib';
-	import { page } from '$app/stores';
+	import { Footer } from '$lib';
 	import type { PageData } from './$types';
-	import { locale } from '$lib/i18n/i18n-svelte';
 
 	export let data: PageData;
 	let { projects } = data;
-
-	const links = ['about', 'skills', 'projects', 'contact-me'];
-	let TabList: HTMLElement | null;
-	let PageRef: Array<HTMLElement>;
-
-	onMount(() => {
-		TabList = document.querySelector('.tab-list');
-		PageRef = document.querySelectorAll('.section') as unknown as Array<HTMLElement>;
-
-		let observer = new IntersectionObserver(
-			(entries) => {
-				entries.forEach((entry) => {
-					if (entry.isIntersecting) {
-						change(links.indexOf(entry.target.id), TabList);
-						change_id(entry.target.id);
-						// console.log(entry.target.id);
-						let hash = links[links.indexOf(entry.target.id)];
-						const url = $page.url;
-						url.hash = hash;
-						window.history.replaceState(null, '', `${$locale}#${hash}`);
-					}
-				});
-			},
-			{ threshold: 0.3 }
-		);
-		PageRef.forEach((element) => {
-			observer.observe(element);
-		});
-
-		return () => {
-			PageRef.forEach((element) => {
-				observer.unobserve(element);
-			});
-		};
-	});
 </script>
 
 <div class="content h-full flex items-center flex-col">
