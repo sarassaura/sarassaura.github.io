a815c3e83438fdbdd7d78b24dbefe799ac53ef69
sarassaura <sarahyukinonakada@gmail.com>
2023-11-24 10:27:17 -0300

    hello motto

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -1,7 +1,14 @@
 <script>
-	// import Mobile from '$lib/assets/images/mobile.png';
-	// import Tablet from '$lib/assets/images/tablet.png';
-	// import Desktop from '$lib/assets/images/desktop.png';
+	import Mobile from '$lib/assets/images/mobile.png';
+	import Tablet from '$lib/assets/images/tablet.png';
+	import Desktop from '$lib/assets/images/desktop.png';
 </script>
 
-<div class="img" />
+<div class="img">
+	<picture>
+		<source media="(min-width: 1024px)" srcset={Desktop} />
+		<source media="(min-width: 640px)" srcset={Tablet} />
+		<source media="(min-width: 0px)" srcset={Mobile} />
+		<img src={Mobile} alt="Showcase" />
+	</picture>
+</div>
