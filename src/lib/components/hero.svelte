6eb1f7e353b9d31fd06eb479c3f3b57c18aedcf5
sarassaura <sarahyukinonakada@gmail.com>
2023-11-24 09:25:47 -0300

    better

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -1,19 +1,7 @@
 <script>
-	import Mobile from '$lib/assets/images/mobile.png';
-	import Tablet from '$lib/assets/images/tablet.png';
-	import Desktop from '$lib/assets/images/desktop.png';
+	// import Mobile from '$lib/assets/images/mobile.png';
+	// import Tablet from '$lib/assets/images/tablet.png';
+	// import Desktop from '$lib/assets/images/desktop.png';
 </script>
 
-<div class="carousel">
-	<div class="slider">
-		<img
-			src={Mobile}
-			alt="Mockup"
-			srcset={`${Mobile} 640w, ${Tablet} 1279w, ${Desktop} 1400w`}
-			sizes={`(max-width: 640px) 237px 190px,
-				(min-width: 640px) 546px 326px,
-				(min-width: 1024px) 270px 540px`}
-			decoding="async"
-		/>
-	</div>
-</div>
+<div class="img" />
