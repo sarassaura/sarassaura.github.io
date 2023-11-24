4b7ed5b609e25522a7178f13ceabc10ed17f2d4c
sarassaura <sarahyukinonakada@gmail.com>
2023-11-24 11:45:04 -0300

    ok

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -9,6 +9,6 @@
 		<source media="(min-width: 1024px)" srcset={Desktop} width="270px" height="540px" />
 		<source media="(min-width: 640px)" srcset={Tablet} width="546px" height="326px" />
 		<source media="(min-width: 0px)" srcset={Mobile} width="322px" height="235px" />
-		<img src={Mobile} alt="Showcase" />
+		<img src={Mobile} alt="Showcase" width="322px" height="235px" />
 	</picture>
 </div>
