b13c17a4dba7a9acc9ac2c19df4ef8cfd559af4a
sarassaura <sarahyukinonakada@gmail.com>
2023-11-24 11:06:46 -0300

    height and width

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -6,9 +6,9 @@
 
 <div class="img">
 	<picture>
-		<source media="(min-width: 1024px)" srcset={Desktop} />
-		<source media="(min-width: 640px)" srcset={Tablet} />
-		<source media="(min-width: 0px)" srcset={Mobile} />
+		<source media="(min-width: 1024px)" srcset={Desktop} width="270px" height="540px" />
+		<source media="(min-width: 640px)" srcset={Tablet} width="546px" height="326px" />
+		<source media="(min-width: 0px)" srcset={Mobile} width="237px" height="190px" />
 		<img src={Mobile} alt="Showcase" />
 	</picture>
 </div>
