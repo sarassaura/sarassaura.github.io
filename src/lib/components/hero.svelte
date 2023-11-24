f9cb2214c1a37b60dd8b7c9e2a5b5993f5e8afa1
sarassaura <sarahyukinonakada@gmail.com>
2023-11-24 08:30:20 -0300

    dont know

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -1,9 +1,19 @@
 <script>
-	import Image from '$lib/assets/images/iphone.webp';
+	import Mobile from '$lib/assets/images/mobile.png';
+	import Tablet from '$lib/assets/images/tablet.png';
+	import Desktop from '$lib/assets/images/desktop.png';
 </script>
 
 <div class="carousel">
 	<div class="slider">
-		<img src={Image} alt="iphone" />
+		<img
+			src={Mobile}
+			alt="Mockup"
+			srcset={`${Mobile} 640w, ${Tablet} 1279w, ${Desktop} 1400w`}
+			sizes={`(max-width: 640px) 237px 190px,
+				(min-width: 640px) 546px 326px,
+				(min-width: 1024px) 270px 540px`}
+			decoding="async"
+		/>
 	</div>
 </div>
