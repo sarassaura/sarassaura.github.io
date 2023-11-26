b1c09d9fcbb6b0005ac6abacb2c17c186bc23318
sarassaura <sarahyukinonakada@gmail.com>
2023-11-26 15:02:07 -0300

    dont know

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -1,14 +1,33 @@
 <script>
-	import Mobile from '$lib/assets/images/mobile.webp';
-	import Tablet from '$lib/assets/images/tablet.webp';
-	import Desktop from '$lib/assets/images/desktop.webp';
+	import { CldImage } from 'svelte-cloudinary';
 </script>
 
 <div class="img">
 	<picture>
-		<source media="(min-width: 1024px)" srcset={Desktop} width="270px" height="540px" />
-		<source media="(min-width: 640px)" srcset={Tablet} width="546px" height="326px" />
-		<source media="(min-width: 0px)" srcset={Mobile} width="322px" height="235px" />
-		<img src={Mobile} alt="Showcase" width="322px" height="235px" />
+		<source
+			media="(min-width: 1024px)"
+			srcset={'https://res.cloudinary.com/drazwv6hx/image/upload/v1701021175/' + 'desktop'}
+			width="270px"
+			height="540px"
+		/>
+		<source
+			media="(min-width: 640px)"
+			srcset={'https://res.cloudinary.com/drazwv6hx/image/upload/v1701021175/' + 'tablet'}
+			width="546px"
+			height="326px"
+		/>
+		<source
+			media="(min-width: 0px)"
+			srcset={'https://res.cloudinary.com/drazwv6hx/image/upload/v1701021175/' + 'mobile'}
+			width="322px"
+			height="235px"
+		/>
+		<CldImage
+			src={'https://res.cloudinary.com/drazwv6hx/image/upload/v1701021175/' + 'mobile'}
+			alt="Showcase"
+			layout="constrained"
+			width="322px"
+			height="235px"
+		/>
 	</picture>
 </div>
