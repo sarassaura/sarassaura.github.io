1f912d7489f3e3cc99b53f10ca09e5b6b2c8aa5f
sarassaura <sarahyukinonakada@gmail.com>
2023-11-29 00:17:05 -0300

    dont know

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const mdsvexOptions = {
 	}
 };
 
-const dev = process.argv.includes('dev');
+// const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -28,9 +28,6 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		paths: {
-			base: dev ? '' : process.env.BASE_PATH
-		},
 		prerender: { crawl: true, entries: ['/', '/en', '/es', '/pt-BR'] }
 	}
 };
