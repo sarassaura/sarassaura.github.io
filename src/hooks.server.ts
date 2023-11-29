31c4388e3cdba7d3d80244d2f8672b2ac5cf95b4
sarassaura <sarahyukinonakada@gmail.com>
2023-11-28 23:31:26 -0300

    dotenv

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,12 @@
+import 'dotenv/config';
 import type { Handle } from '@sveltejs/kit';
 import { detectLocale } from '$lib/i18n/i18n-util';
 import { sequence } from '@sveltejs/kit/hooks';
-import { GITHUB } from '$env/static/private';
 import { github } from '$lib';
 
-const projects = await github(GITHUB);
+const credentials: string = process.env.GITHUB as string;
+
+const projects = await github(credentials);
 
 const i18n = (async ({ event, resolve }) => {
 	const locale = detectLocale(() => [event.params.lang ?? '']);
